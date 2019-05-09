be3498b65daab2048c174c4e6968a687f4c14297
FilaCo <filaco@dchv-pc.filaco.org>
2019-05-10 03:09:41 +0500

    Added an implementation of task, where the percentage concentration is calculated from a mass of the substance and a mass of the solution before vaporizing

---
+++ PCApp/PCAppSolver/SolverMassSub_Sol.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace PCAppSolver
+{
+    /// <summary>
+    /// Finds the percentage concentration from a mass of the substance and a mass of the vaporized solution
+    /// </summary>
+    public class SolverMassSub_Sol:ISolver
+    {
+        private double _massSubstance;
+        private double _massSol;
+        
+        public double MassSubstance
+        {
+            get => _massSubstance;
+            set
+            {
+                if (value < 0)
+                    throw new Exception("Mass cannot be negative");
+                _massSubstance = value;
+            }
+        }
+
+        /// <summary>
+        /// Mass of the solution before vaporizing
+        /// </summary>
+        /// <exception cref="Exception"></exception>
+        public double MassSol
+        {
+            get => _massSol;
+            set
+            {
+                if (value < 0)
+                    throw new Exception("Mass cannot be negative");
+                _massSol = value;
+            }
+        }
+
+        public double Execute()
+        {
+            return MassSubstance / MassSol;
+        }
+    }
+}
